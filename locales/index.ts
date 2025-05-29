50fd35f068125fa192b38d0d84ff478ecb664016
kongxh <2180733045@qq.com>
2025-05-29 11:02:38 +0800

    feat(项目初始化): ElementPlus 国际化全局配置

--- locales/index.ts
+++ locales/index.ts
@@ -1,6 +1,6 @@
 import epEn from 'element-plus/es/locale/lang/en'
-import epZh from 'element-plus/es/locale/lang/zh-tw'
+import epZh from 'element-plus/es/locale/lang/zh-cn'
 import en from './en.ts'
-import zh from './zh.ts'
+import zh from './zh-cn.ts'
 
 export { epEn, epZh, en, zh }
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n'
 import { RouterLink, RouterView } from 'vue-router'
+import { langsConfig } from './plugins'
+
+const { locale } = useI18n()
+
 
 /** Element 全局配置 */
 const ElementFormDefaultSize: 'large' | 'default' | 'small' = 'default'
 const ElementDialogZIndex = 2000
+const localeMessage = computed(() => locale.value === 'en' ? langsConfig.en : langsConfig.zh)
+
 </script>
 
 <template>
-    <el-config-provider :size="ElementFormDefaultSize" :z-index="ElementDialogZIndex">
+    <el-config-provider :size="ElementFormDefaultSize" :z-index="ElementDialogZIndex" :locale="localeMessage">
         <RouterView />
     </el-config-provider>
 </template>
