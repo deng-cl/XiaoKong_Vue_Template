a15a40680a093eb94758402f11324d419fb5654b
kongxh <2180733045@qq.com>
2025-08-27 18:38:26 +0800

    fix(utils/store): 调整 pinia 持久化存储使用自定义存储库(xk-storage)

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,10 +1,22 @@
+import { storage } from '@/utils/storager'
 import { createPinia } from 'pinia'
 import { createPersistedState } from 'pinia-plugin-persistedstate'
 
 export const registerPinia = () => {
     return createPinia().use(
         createPersistedState({
-            key: (id) => `__Hpbs-${id}__`, // -- 配置全局持久化缓存 key
+            // key: (id) => `__Hpbs-${id}__`, // -- 配置全局持久化缓存 key
+
+            /* 使用自定义 storage 存储器 */
+            storage: {
+                getItem: storage.get.bind(storage),
+                setItem: storage.set.bind(storage),
+            },
+            /* 自定义持久化插件序列化操作（取消该插件的自动序列化）→ 因为与所封装的 HStorage 工具的序列化有冲突，所以禁用该默认的序列化功能 */
+            serializer: {
+                serialize: (value) => value as any,
+                deserialize: (value) => value as any,
+            },
         }),
     )
 }
